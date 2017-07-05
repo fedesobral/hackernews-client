40caaf0f09a7e83eebb87588fd0255515d5c9a58
Federico Sobral <fsobral@xseed.com.uy>
2017-07-05 17:46:17 -0300

    Testing different matchers just for fun

--- src/components/Add/test.js
+++ src/components/Add/test.js
@@ -14,3 +14,17 @@ describe("Add", () => {
 		expect(Add(1, Add(2, 3))).toBe(Add(Add(1, 2), 3));
 	});
 });
+
+describe("Testing Matchers", () => {
+	it("Two plus Three equals Five", () => {
+		expect(2 + 3).toBe(5);
+	});
+	it("Object equality", () => {
+		const obj = { a: 1 };
+		obj['b'] = 2;
+		expect(obj).toEqual({ a: 1, b: 2 });
+	});
+	it("Distributive property", () => {
+		expect(Add(1, Add(2, 3))).toBe(Add(Add(1, 2), 3));
+	});
+});
